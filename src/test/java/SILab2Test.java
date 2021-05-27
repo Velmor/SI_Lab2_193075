9acfc6bf172a0394cfc07215be56a22f55895bef
Ines Vlemor <inesvelmor@yahoo.com>
2021-05-27 23:36:31 +0200

    193075

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-//193075
+//193075 Ines
 
 import static org.junit.jupiter.api.Assertions.*;
 
